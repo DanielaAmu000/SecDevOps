4e21f18ba7f81f18a3f8e57cb3b8dd365b7cc75e
DanielaAmu000 <danielaamu7@gmail.com>
2023-06-23 10:18:43 -0500

    Parchear archivo y añadir writeup

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ function validateLogin(event) {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     
-    if (username !== "admin") {
-      document.getElementById("message").innerHTML = "Usuario incorrecto";
-    } else if (password !== "contraseña") {
-      document.getElementById("message").innerHTML = "Contraseña incorrecta";
-    } else {
+    if (username == "admin" && password == "SecDevOps_PC$$") {
       document.getElementById("message").innerHTML = "Acceso autorizado";
+    } else{
+      document.getElementById("message").innerHTML = "Usuario  o contraseña incorrectos";
     }
-  }
-  
+}
